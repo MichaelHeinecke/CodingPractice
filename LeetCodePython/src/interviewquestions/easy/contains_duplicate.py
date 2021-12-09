89e9b9c1d0a6e4636946848d01f99492bdc00f0f
Michael Heinecke <michael.heinecke.1337@gmail.com>
2021-12-09 14:57:04 +0100

    Implement test and fix solution

--- LeetCodePython/src/interviewquestions/easy/contains_duplicate.py
+++ LeetCodePython/src/interviewquestions/easy/contains_duplicate.py
@@ -4,7 +4,7 @@ from typing import List
 
 def containsDuplicate(nums: List[int]) -> bool:
     counter = collections.Counter(nums)
-    for k, v in counter:
+    for k, v in counter.items():
         if v > 1:
             return True
     return False
--- LeetCodePython/test/interviewquestions/easy/test_contains_duplicate.py
+++ LeetCodePython/test/interviewquestions/easy/test_contains_duplicate.py
@@ -1,7 +1,12 @@
+from interviewquestions.easy.contains_duplicate import containsDuplicate
+
+
 def test_contains_duplicate_duplicate_returns_true():
     nums = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
-    return
+    assert containsDuplicate(nums) is True
+
 
 
 def test_contains_duplicate_no_duplicate_returns_false():
     nums = [1, 2, 3, 4]
+    assert containsDuplicate(nums) is False
