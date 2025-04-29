164820c46762a9b7084921f92bb72a04499ec5e9
MichaelHeinecke <47566635+MichaelHeinecke@users.noreply.github.com>
2025-04-29 12:41:35 +0200

    use defaultdict for user aggregate

--- DataEngineeringProblems/parsing_json.py
+++ DataEngineeringProblems/parsing_json.py
@@ -7,28 +7,26 @@
 # The last event timestamp
 # Output a dictionary (or JSON) with the aggregated information.
 import json
+from collections import defaultdict
 from datetime import datetime
 
 def aggregate_events(event_data: list[dict[str, str]]) -> dict[str, dict[str, int | datetime]]:
-    agg = {}
+    agg = defaultdict(lambda: {
+        'total_activities': 0,
+        'first_event': None,
+        'last_event': None,
+    })
 
     for e in event_data:
         user = e.get('user_id',)
-        # event = e.get('event')
+        event = e.get('event')
         timestamp_str = e.get('timestamp')
         timestamp = datetime.strptime(timestamp_str, "%Y-%m-%dT%H:%M:%SZ")
 
-        if user not in agg:
-            agg[user] = {
-                'total_activities': 1,
-                'first_event': timestamp,
-                'last_event': timestamp,
-            }
-        else:
-            agg_user = agg.get(user)
-            agg_user['total_activities'] += 1
-            agg_user['first_event'] = min(timestamp, agg_user['first_event'])
-            agg_user['last_event'] = max(timestamp, agg_user['last_event'])
+        agg_user = agg[user]
+        agg_user['total_activities'] += 1
+        agg_user['first_event'] = timestamp if not agg_user['first_event'] else min(timestamp, agg_user['first_event'])
+        agg_user['last_event'] = timestamp if not agg_user['last_event'] else max(timestamp, agg_user['last_event'])
 
     return agg
 
